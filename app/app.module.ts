c3d2cb9bf23f90f183454f3fc0cbc75a97c5ba6e
Vladimir <vlad@simplytechnologies.net>
2016-11-21 18:50:55 +0400

    Angular2_training

--- app/app.module.ts
+++ app/app.module.ts
@@ -1,13 +1,53 @@
 import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule }   from '@angular/forms';
+import { RouterModule } from '@angular/router';
 
-import { AppComponent }   from './app.component';
+import { HeroesComponent }   from './heroes.component';
 import { HeroDetailComponent } from './hero-detail.component';
+import { AppComponent} from "./app.component";
+import { DashboardComponent } from "./dashboard.component"
+import { HeroService } from './hero.service';
+
+
+
+
 
 @NgModule({
-    imports: [BrowserModule, FormsModule],
-    declarations: [ AppComponent, HeroDetailComponent ],
-    bootstrap:    [ AppComponent ]
+
+    imports: [
+        BrowserModule,
+        FormsModule,
+        RouterModule.forRoot([
+            {
+                path: '',
+                redirectTo: 'dashboard',
+                pathMatch: 'full'
+            },
+            {
+              path: 'dashboard',
+              component: DashboardComponent
+            },
+            {
+                path: 'heroes',
+                component: HeroesComponent
+            },
+            {
+                path: 'datail/:id',
+                component: HeroDetailComponent
+            }
+        ])
+    ],
+    declarations: [
+        AppComponent,
+        HeroesComponent,
+        DashboardComponent,
+        HeroDetailComponent
+    ],
+    providers: [HeroService],
+    bootstrap: [ AppComponent ]
+
 })
-export class AppModule { }
+export class AppModule {
+
+}
